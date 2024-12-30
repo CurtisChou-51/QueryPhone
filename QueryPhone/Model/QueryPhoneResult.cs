7105d866804c4018d49958ec71b6ff171a994300
acc2git <acc2gml@gmail.com>
2024-12-30 11:28:04 +0800

    使用httpClientFactory、調整TellowsClient、調整註解

--- QueryPhone/Model/QueryPhoneResult.cs
+++ QueryPhone/Model/QueryPhoneResult.cs
@@ -6,8 +6,10 @@
 
         public bool Success { get; set; }
 
+        /// <summary> 用戶回報 </summary>
         public IEnumerable<string> ReportMsgs { get; set; } = Enumerable.Empty<string>();
 
+        /// <summary> 總評 </summary>
         public IEnumerable<string> SummaryMsgs { get; set; } = Enumerable.Empty<string>();
     }
 }
